879bba6b6be4c05692dd37d322c0c50c3d03d070
Camilo <camilopb0405@gmail.com>
2026-01-28 21:48:45 -0500

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+//todas las funciones de react que empiezan por "use" son hooks de react
+//tecnicamente son como cualquier otra funcion pero solo deben ser llamadas dentro
+// de react component functions y en el tope del componente (no dentro de condicionales o loops)
 import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS as concepts } from "./data";
 import Header from "./Components/Header/Header";
 import CoreConcept from "./Components/CoreConcept";
 import TabButton from "./Components/TabButton";
+import { EXAMPLES } from "./data";
 // Los props son para pasar información/data entre componentes y poder usarla en distintos componentes
 //Ejemplo
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  //dentro de useState, el primer valor es el valor inicial del state, es decir el que se renderiza la primera vez
+  //que el componente despliega en la UI
+
+  //una buena practica es usar el mismo nombre para el state y la function que lo actualiza "selectedTopic" y "setSelectedTopic"
+  //NOTA: selectedTopic es solo una variable normal, en donde se alamcena el valor default del state, el cual podrá cambiar gracias
+  //a la function setSelectedTopic
+  // dato curioso: la razon por la cual se puede usar const y no necesariamente let es porque
+  // lo que hace el useStae es cargar de nuevo el componente cada vez que el state cambia, es como darle refresh al componente
   function handleClick(input) {
     //los inputs que esta function debe recibir son "Components", "JSX", "Props" y "State".
     // dependiendo del input que reciba, el contenido dentro de "Dynamic Content" debe cambiar
-    console.log(input);
+
+    //NOTA: By default los react components solo se ejecutan una vez
+    //por lo tanto, si quiero que algo cambie en la UI debo usar "State" para que react se de cuenta
+    // que algo cambió y vuelva a renderizar el componente
+    setSelectedTopic(input);
   }
   return (
     <div>
@@ -51,7 +69,7 @@ function App() {
 
             {/* Parte 2:
              Ahora para poder hacer que el contenido cambie segun el boton seleccionado */}
-            <TabButton onSelect={() => handleClick("componets")}>
+            <TabButton onSelect={() => handleClick("components")}>
               Components
             </TabButton>
             <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
@@ -62,7 +80,23 @@ function App() {
             si no la metemos dentro de la arrow function, al poner solo "handleClick()" hace que la funcion se ejecute cuando la linea de codigo <TabButton> se ejecute.
             */}
           </menu>
-          Dynamic Content
+          {/* para poder mostrar un mensaje inicial cuando ningun boton ha sido seleccionado necesitamos hacer un
+          consitional rendering */}
+          {/* selectedTopic === null es lo mismo que escribir !selectedTopic es como decir "selectedTopic es null" , con la diferencia 
+          que si uso selectedTopic === null, entonces el valor inicial que debo poner a selectedTopic 
+          es null, mientras que !selectedTopic es una expresion booleana puedo dejar el useSate con
+           valor inicial vacío*/}
+          {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
